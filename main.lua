8e76898911786a3dd577e395c69625c3ae36f26a
gabrieldkk0018 <davidkaik0018@gmail.com>
2025-12-23 17:14:55 -0300

    Implement main loader for MH Universal
    Added a main loader for MH Universal that loads various modules including config, GUI, ESP, and Aimbot.

--- main.lua
+++ main.lua
@@ -1,1 +1,24 @@
+-- MH Universal
+-- Loader principal
 
+local BASE_URL = "https://raw.githubusercontent.com/gabrieldkk0018/Mh-Universal/main/"
+
+local function load(path)
+    local url = BASE_URL .. path
+    local src = game:HttpGet(url)
+    return loadstring(src)()
+end
+
+-- Config
+load("config.lua")
+
+-- GUI
+load("gui/gui.lua")
+
+-- ESP
+load("esp/esp.lua")
+
+-- Aimbot
+load("aimbot/aimbot.lua")
+
+print("[MH Universal] Carregado com sucesso")
